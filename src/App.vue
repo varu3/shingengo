e5f346c38833b39c5c89dd1aca9bfb988f624230
nakamura_subaru <subaru.nakamura@drecom.co.jp>
2019-04-04 23:14:23 +0900

    InitialCommit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <section>
+      <div class="columns is-centered">
+        <div class="column is-three-fifths is-offset-one-fifth">
+          <SynthesizedImage :msg="msg"/>
+          <Form @input="onInput" />
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import SynthesizedImage from './components/SynthesizedImage'
+import Form from  './components/Form'
+import 'bulma/css/bulma.css'
+
+export default {
+  name: 'App',
+  components: {
+    SynthesizedImage,
+    Form
+  },
+  data() {
+    return {
+      msg: '',
+    }
+  },
+  methods: {
+    onInput(newMessage) {
+      this.msg = newMessage
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="level-item">
+      <input class="input is-small" type="text" :value="msg" @change="onChangeInput">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: ''
+    }
+  },
+  methods: {
+    onChangeInput(event) {
+      this.commitChange(event.target.value)
+    },
+    commitChange(newValue) {
+      this.$emit('input', newValue)
+    }
+  }
+}
+
+</script>
+
